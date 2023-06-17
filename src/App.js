dee4738c3618310d44715e60a66c0b13289a213b
Mrinal Rai <105412590+RaiMrina@users.noreply.github.com>
2023-06-17 20:09:51 +0530

    Code done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h1>Admission Form</h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="address">Address:</label>
+        <textarea id="address" name="address" />
+
+        <label htmlFor="dob">Date of Birth:</label>
+        <input type="date" id="dob" name="dob" />
+
+        <label htmlFor="prevSchool">Previous School Name:</label>
+        <input type="text" id="prevSchool" name="prevSchool" />
+
+        <label htmlFor="tcId">Transfer Certificate ID:</label>
+        <input type="text" id="tcId" name="tcId" />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
